218161515c2eb14257b021bdbeea9aadc4fb877a
YOONJI <jelliijoa@gmail.com>
2023-04-21 03:20:08 +0900

    #2 [WEEK2] 기본 과제 제출

--- Week2/Seminar/SecondSeminar/src/main/java/sopt/org/SecondSeminar/domain/Posting.java
+++ Week2/Seminar/SecondSeminar/src/main/java/sopt/org/SecondSeminar/domain/Posting.java
@@ -1,2 +1,32 @@
-package sopt.org.SecondSeminar.domain;public class Posting {
+package sopt.org.SecondSeminar.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Posting {
+    private Long id;
+    private String title;
+    private String context;
+    private String writer;
+    private String time;
+
+    public Posting(String title, String context, String writer, String time) {
+        this.title = title;
+        this.context = context;
+        this.writer = writer;
+        this.time = time;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + this.id + "\n" +
+                "title: " + this.title + "\n" +
+                "context: " + this.context + "\n" +
+                "writer: " + this.writer + "\n" +
+                "time: " + this.time;
+    }
 }
