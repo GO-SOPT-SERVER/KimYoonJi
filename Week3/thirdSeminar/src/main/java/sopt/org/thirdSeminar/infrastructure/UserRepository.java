5c3f5cf51a3f129777663d5854a7fd9604935a93
YOONJI <jelliijoa@gmail.com>
2023-05-26 18:39:20 +0900

    [WEEK6] 기본 과제 제출

--- Week3/thirdSeminar/src/main/java/sopt/org/thirdSeminar/infrastructure/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package sopt.org.thirdSeminar.infrastructure;
-
-import org.springframework.data.repository.Repository;
-import sopt.org.thirdSeminar.domain.User;
-
-public interface UserRepository extends Repository<User, Long> {
-    void save(User user);
-}
